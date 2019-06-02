ca5fb85148ae27d7a93c1806cda79637bb612f9f
hunterkillzero <snguoila@yahoo.com>
2019-06-03 00:43:31 +0700

    need to fix gen array

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <stdlib.h>
 #include <iomanip>
+#include <cstring>
 
 using namespace std;
 
@@ -94,6 +95,9 @@ void merge(int arr[], int left, int mid, int right ) {
         j++;
         k++;
     }
+
+    delete[] L;
+    delete[] R;
 }
 
 void mergeSort(int arr[], int left, int right) {
@@ -158,16 +162,38 @@ void quickSortOptimize(int arr[], int left, int right) {
     }
 }
 
-
 void QuickSort(int arr[], int n) {
     quickSort(arr, 0, n);
+}
 
+void CountingSort(int arr[], int n){
+    int *new_arr = new int[n];
+    memset(new_arr, 0, n);
+
+    for(int i = 0; i < n; i++){
+        new_arr[arr[i]]++;
+    }
+
+    int index = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = new_arr[i]; j > 0; j--) {
+            arr[index] = i;
+            index++;
+        }
+    }
+    delete[] new_arr;
 }
 
 int main() {
     srand(time(0));
 
-    int n = 100000;
+    //int n = 1000;
+    //int n = 10000;
+    int n = 100000; //good test  case 0
+    //int n = 1000000; //good test  case 1
+    //int n = 10000000; // merger + CountingSort
+
+    //int n = 10;
     int *arr   = new int[n];
     int *arr_1 = new int[n];
     int *arr_2 = new int[n];
@@ -175,27 +201,32 @@ int main() {
     int *arr_4 = new int[n];
     int *arr_5 = new int[n];
     int *arr_6 = new int[n];
+    // int *arr_7 = new int[n];
+    // int *arr_8 = new int[n];
+    // int *arr_9 = new int[n];
+    // int *arr_10 = new int[n];
 
 
     for(int i = 0; i < n; i++) {
-        arr[i] = rand() % 1000 - 10;
+        arr[i] = rand() % 1000;
         arr_1[i] = arr[i];
         arr_2[i] = arr[i];
         arr_3[i] = arr[i];
         arr_4[i] = arr[i];
         arr_5[i] = arr[i];
         arr_6[i] = arr[i];
+        // arr_7[i] = arr[i];
+        // arr_8[i] = arr[i];
+        // arr_9[i] = arr[i];
+        // arr_10[i] = arr[i];
     }
 
-    //printArray(arr, n);
-    //InsertionSort(arr, n);
-    //SelectionSort(arr, n);
-
-    //BubbleSort(arr, n);
-    //MergeSort(arr, n);
-    //printArray(arr, n);
+    cerr << "Starting" << "............." << '\n';
+    cerr << "N = " << n << '\n';
     clock_t beginTime, endTime;
     double elapsedTime;
+
+/////////////////////////////////////////////////////////////////////////////////
     beginTime = clock();
     InsertionSort(arr, n);
     endTime = clock();
@@ -232,6 +263,13 @@ int main() {
     endTime = clock();
     elapsedTime = (double) (endTime - beginTime);
     cerr << "Time elapsed SelectionSort: " <<  setprecision(10) <<elapsedTime / CLOCKS_PER_SEC << " s.\n" << endl;
+
+
+    beginTime = clock();
+    CountingSort(arr_6, n);
+    endTime = clock();
+    elapsedTime = (double) (endTime - beginTime);
+    cerr << "Time elapsed CountingSort: " <<  setprecision(10) <<elapsedTime / CLOCKS_PER_SEC << " s.\n" << endl;
 
 
 
